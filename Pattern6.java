75cf62d80a6c958f1017bd1b79e3082a1c5a177a
ABHISHEK-B <etamabhishek000@gmail.com>
2025-07-28 14:39:07 +0530

    Create Pattern6.java

---
+++ Pattern6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        sc.close();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Enter number of rows: 5
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
