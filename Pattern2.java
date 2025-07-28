07f072496c5142fd557b2cd6adb521bb18355413
Abhisheketam01 <etamabhishek000@gmail.com>
2025-07-28 13:58:06 +0530

    done

--- Pattern2.java
+++ Pattern2.java
@@ -1,31 +1,3 @@
-// import java.util.Scanner;
-
-// public class Pattern2 {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-
-//         System.out.print("Enter number of rows: ");
-//         int rows = sc.nextInt();
-
-//         System.out.print("Enter number of columns: ");
-//         int cols = sc.nextInt();
-//         sc.close();
-
-//         for (int i = 1; i <= rows; i++) {
-//             for (int j = 1; j <= cols; j++) {
-//                 // Boundary condition
-//                 if (i == 1 || j == 1 || j == cols || i == rows) {
-//                     System.out.print("* ");
-                    
-//                 } else {
-//                     System.out.print("  "); // 2 spaces for alignment
-//                 }
-//             }
-//             System.out.println(); // New line after each row
-//         }
-//     }
-// }
-
 import java.util.Scanner;
 
 public class Pattern2{
